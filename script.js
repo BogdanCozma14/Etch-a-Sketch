cc925c6bcc775a48f4510a9a05f975c85e698477
Bogdan Cozma <[bcozma61@yahoo.com]>
2024-08-31 21:41:21 +0300

    changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+// squarecolor declared globally to be accessible in the functions
+let squareColor = "black";
 document.addEventListener("DOMContentLoaded", function() {
     // the default values for the number of grids is 16
     let rows = 16;
@@ -9,11 +10,47 @@ document.addEventListener("DOMContentLoaded", function() {
     let gridWidth = grid.offsetWidth;
     console.log(gridHeight);
     console.log(gridWidth);
+    // track selecte color (the default would be black)
+    const favcolor = document.querySelector("#favcolor");
+    favcolor.addEventListener("change", function(e){
+        squareColor = e.target.value;
+    })
     // creating the default grid with 16x16 squares
     createGrid(rows, gridHeight, gridWidth)
     // the button for changing the number of grids
     const changeGridButton = document.querySelector("#changeGrid");
     changeGridButton.addEventListener("click", ChangeGrid);
+    // rainbow button
+    const rainbow = document.querySelector("#rainbow");
+    // we check or uncheck the button
+    rainbow.addEventListener("click", function() {
+        if(rainbow.classList.contains("active")){
+            rainbow.classList.remove("active");
+        }
+        else {
+            rainbow.classList.add("active");
+        }
+    })
+    // eraser button
+    const eraser = document.querySelector("#eraser");
+    eraser.addEventListener("click", function(){
+        if(eraser.classList.contains("active")){
+            eraser.classList.remove("active");
+        }
+        else {
+            eraser.classList.add("active");
+        }
+    })
+    // clear button
+    const clearButton = document.querySelector("#clear");
+    clearButton.addEventListener("click", function() {
+        // selecting all the squares
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
+            square.style.backgroundColor = "white";
+        });
+    });
+
 })
 
 // function for creating the grid
@@ -22,6 +59,8 @@ function createGrid(numberOfRows, gridHeight, gridWidth) {
     // grid.innerHTML = ""; // clear existing grid first
     let squareHeight = (gridHeight / numberOfRows).toFixed(2);
     // creating all necessary grids
+    const rainbow = document.querySelector("#rainbow");
+    const eraser = document.querySelector("#eraser");
     for(let i = 0; i < numberOfRows; i++)
     {
         const newrow = document.createElement("div");
@@ -34,11 +73,29 @@ function createGrid(numberOfRows, gridHeight, gridWidth) {
             newsquare.style.height = squareHeight + "px";
             newsquare.style.width = squareHeight + "px";
             newsquare.classList.add("square");
+            newsquare.addEventListener("mouseover", function() {
+                // verify if rainbow button is active
+                if (eraser.classList.contains("active")) {
+                    newsquare.style.backgroundColor = "white";
+                }
+                else if (rainbow.classList.contains("active")) {
+                    // we get 3 random values for the rgb values
+                    let rgb1 = Math.round(Math.random() * 255);
+                    let rgb2 = Math.round(Math.random() * 255);
+                    let rgb3 = Math.round(Math.random() * 255);
+                    newsquare.style.backgroundColor = 'rgb('+ rgb1 + ',' + rgb2 + ',' + rgb3 +')';
+                }
+                else {
+                    newsquare.style.backgroundColor = squareColor;
+                }
+            })
             newrow.appendChild(newsquare);
         }
         grid.appendChild(newrow);
     }
 }
+
+// function for changing the grid
 function ChangeGrid() {
     let gridsNumber = parseInt(prompt("Choose the number of grids"));
     // verifying that the user prompted a value between 1 and 100
